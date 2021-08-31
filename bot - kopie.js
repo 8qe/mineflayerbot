71da7d62f811840d98d1fdc40406c52f618cf53e
8qe <87573618+8qe@users.noreply.github.com>
2021-08-31 21:38:04 +0200

    Add files via upload

---
+++ bot - kopie.js
@@ -0,0 +1,706 @@
+const mineflayer = require('mineflayer')
+let ip = "lolQuitxD.aternos.me"
+let porter = "24156"
+var Prefix = "MCLORD_"
+var Random1 = Math.floor(Math.random() * 1000000) + 1;
+var User1 = Prefix+Random1
+var Random2 = Math.floor(Math.random() * 1000000) + 1;
+var User2 = Prefix+Random2
+var Random3 = Math.floor(Math.random() * 1000000) + 1;
+var User3 = Prefix+Random3
+var Random4 = Math.floor(Math.random() * 1000000) + 1;
+var User4 = Prefix+Random4
+var Random5 = Math.floor(Math.random() * 1000000) + 1;
+var User5 = Prefix+Random5
+var Random6 = Math.floor(Math.random() * 1000000) + 1;
+var User6 = Prefix+Random6
+var Random7 = Math.floor(Math.random() * 1000000) + 1;
+var User7 = Prefix+Random7
+var Random8 = Math.floor(Math.random() * 1000000) + 1;
+var User8 = Prefix+Random8
+var Random9 = Math.floor(Math.random() * 1000000) + 1;
+var User9 = Prefix+Random9
+var Random10 = Math.floor(Math.random() * 1000000) + 1;
+var User10 = Prefix+Random10
+var Random11 = Math.floor(Math.random() * 1000000) + 1;
+var User11 = Prefix+Random11
+var Random12 = Math.floor(Math.random() * 1000000) + 1;
+var User12 = Prefix+Random12
+var Random13 = Math.floor(Math.random() * 1000000) + 1;
+var User13 = Prefix+Random13
+var Random14 = Math.floor(Math.random() * 1000000) + 1;
+var User14 = Prefix+Random14
+var Random15 = Math.floor(Math.random() * 1000000) + 1;
+var User15 = Prefix+Random15
+var Random16 = Math.floor(Math.random() * 1000000) + 1;
+var User16 = Prefix+Random16
+var Random17 = Math.floor(Math.random() * 1000000) + 1;
+var User17 = Prefix+Random17
+var Random18 = Math.floor(Math.random() * 1000000) + 1;
+var User18 = Prefix+Random18
+var Random19 = Math.floor(Math.random() * 1000000) + 1;
+var User19 = Prefix+Random19
+var Random20 = Math.floor(Math.random() * 1000000) + 1;
+var User20 = Prefix+Random20
+var Random21 = Math.floor(Math.random() * 1000000) + 1;
+var User21 = Prefix+Random21
+var Random22 = Math.floor(Math.random() * 1000000) + 1;
+var User22 = Prefix+Random22
+var Random23 = Math.floor(Math.random() * 1000000) + 1;
+var User23 = Prefix+Random23
+var Random24 = Math.floor(Math.random() * 1000000) + 1;
+var User24 = Prefix+Random24
+var Random25 = Math.floor(Math.random() * 1000000) + 1;
+var User25 = Prefix+Random25
+var Random26 = Math.floor(Math.random() * 1000000) + 1;
+var User26 = Prefix+Random26
+var Random27 = Math.floor(Math.random() * 1000000) + 1;
+var User27 = Prefix+Random27
+var Random28 = Math.floor(Math.random() * 1000000) + 1;
+var User28 = Prefix+Random28
+var Random29 = Math.floor(Math.random() * 1000000) + 1;
+var User29 = Prefix+Random29
+var Random30 = Math.floor(Math.random() * 1000000) + 1;
+var User30 = Prefix+Random30
+var Random31 = Math.floor(Math.random() * 1000000) + 1;
+var User31 = Prefix+Random31
+var Random32 = Math.floor(Math.random() * 1000000) + 1;
+var User32 = Prefix+Random32
+var Random33 = Math.floor(Math.random() * 1000000) + 1;
+var User33 = Prefix+Random33
+var Random34 = Math.floor(Math.random() * 1000000) + 1;
+var User34 = Prefix+Random34
+var Random35 = Math.floor(Math.random() * 1000000) + 1;
+var User35 = Prefix+Random35
+var Random36 = Math.floor(Math.random() * 1000000) + 1;
+var User36 = Prefix+Random36
+var Random37 = Math.floor(Math.random() * 1000000) + 1;
+var User37 = Prefix+Random37
+var Random38 = Math.floor(Math.random() * 1000000) + 1;
+var User38 = Prefix+Random38
+var Random39 = Math.floor(Math.random() * 1000000) + 1;
+var User39 = Prefix+Random39
+var Random40 = Math.floor(Math.random() * 1000000) + 1;
+var User40 = Prefix+Random40
+var Random41 = Math.floor(Math.random() * 1000000) + 1;
+var User41 = Prefix+Random41
+var Random42 = Math.floor(Math.random() * 1000000) + 1;
+var User42 = Prefix+Random42
+var Random43 = Math.floor(Math.random() * 1000000) + 1;
+var User43 = Prefix+Random43
+var Random44 = Math.floor(Math.random() * 1000000) + 1;
+var User44 = Prefix+Random44
+var Random45 = Math.floor(Math.random() * 1000000) + 1;
+var User45 = Prefix+Random45
+var Random46 = Math.floor(Math.random() * 1000000) + 1;
+var User46 = Prefix+Random46
+var Random47 = Math.floor(Math.random() * 1000000) + 1;
+var User47 = Prefix+Random47
+var Random48 = Math.floor(Math.random() * 1000000) + 1;
+var User48 = Prefix+Random48
+var Random49 = Math.floor(Math.random() * 1000000) + 1;
+var User49 = Prefix+Random49
+var Random50 = Math.floor(Math.random() * 1000000) + 1;
+var User50 = Prefix+Random50
+var Random51 = Math.floor(Math.random() * 1000000) + 1;
+var User51 = Prefix+Random51
+var Random52 = Math.floor(Math.random() * 1000000) + 1;
+var User52 = Prefix+Random52
+var Random53 = Math.floor(Math.random() * 1000000) + 1;
+var User53 = Prefix+Random53
+var Random54 = Math.floor(Math.random() * 1000000) + 1;
+var User54 = Prefix+Random54
+var Random55 = Math.floor(Math.random() * 1000000) + 1;
+var User55 = Prefix+Random55
+var Random56 = Math.floor(Math.random() * 1000000) + 1;
+var User56 = Prefix+Random56
+var Random57 = Math.floor(Math.random() * 1000000) + 1;
+var User57 = Prefix+Random57
+var Random58 = Math.floor(Math.random() * 1000000) + 1;
+var User58 = Prefix+Random58
+var Random59 = Math.floor(Math.random() * 1000000) + 1;
+var User59 = Prefix+Random59
+var Random60 = Math.floor(Math.random() * 1000000) + 1;
+var User60 = Prefix+Random60
+var Random61 = Math.floor(Math.random() * 1000000) + 1;
+var User61 = Prefix+Random61
+var Random62 = Math.floor(Math.random() * 1000000) + 1;
+var User62 = Prefix+Random62
+var Random63 = Math.floor(Math.random() * 1000000) + 1;
+var User63 = Prefix+Random63
+var Random64 = Math.floor(Math.random() * 1000000) + 1;
+var User64 = Prefix+Random64
+var Random65 = Math.floor(Math.random() * 1000000) + 1;
+var User65 = Prefix+Random65
+var Random66 = Math.floor(Math.random() * 1000000) + 1;
+var User66 = Prefix+Random66
+var Random67 = Math.floor(Math.random() * 1000000) + 1;
+var User67 = Prefix+Random67
+var Random68 = Math.floor(Math.random() * 1000000) + 1;
+var User68 = Prefix+Random68
+var Random69 = Math.floor(Math.random() * 1000000) + 1;
+var User69 = Prefix+Random69
+var Random70 = Math.floor(Math.random() * 1000000) + 1;
+var User70 = Prefix+Random70
+var Random71 = Math.floor(Math.random() * 1000000) + 1;
+var User71 = Prefix+Random71
+var Random72 = Math.floor(Math.random() * 1000000) + 1;
+var User72 = Prefix+Random72
+var Random73 = Math.floor(Math.random() * 1000000) + 1;
+var User73 = Prefix+Random73
+var Random74 = Math.floor(Math.random() * 1000000) + 1;
+var User74 = Prefix+Random74
+var Random75 = Math.floor(Math.random() * 1000000) + 1;
+var User75 = Prefix+Random75
+var Random76 = Math.floor(Math.random() * 1000000) + 1;
+var User76 = Prefix+Random76
+var Random77 = Math.floor(Math.random() * 1000000) + 1;
+var User77 = Prefix+Random77
+var Random78 = Math.floor(Math.random() * 1000000) + 1;
+var User78 = Prefix+Random78
+var Random79 = Math.floor(Math.random() * 1000000) + 1;
+var User79 = Prefix+Random79
+var Random80 = Math.floor(Math.random() * 1000000) + 1;
+var User80 = Prefix+Random80
+var Random81 = Math.floor(Math.random() * 1000000) + 1;
+var User81 = Prefix+Random81
+var Random82 = Math.floor(Math.random() * 1000000) + 1;
+var User82 = Prefix+Random82
+var Random83 = Math.floor(Math.random() * 1000000) + 1;
+var User83 = Prefix+Random83
+var Random84 = Math.floor(Math.random() * 1000000) + 1;
+var User84 = Prefix+Random84
+var Random85 = Math.floor(Math.random() * 1000000) + 1;
+var User85 = Prefix+Random85
+var Random86 = Math.floor(Math.random() * 1000000) + 1;
+var User86 = Prefix+Random86
+var Random87 = Math.floor(Math.random() * 1000000) + 1;
+var User87 = Prefix+Random87
+var Random88 = Math.floor(Math.random() * 1000000) + 1;
+var User88 = Prefix+Random88
+var Random89 = Math.floor(Math.random() * 1000000) + 1;
+var User89 = Prefix+Random89
+var Random90 = Math.floor(Math.random() * 1000000) + 1;
+var User90 = Prefix+Random90
+var Random91 = Math.floor(Math.random() * 1000000) + 1;
+var User91 = Prefix+Random91
+var Random92 = Math.floor(Math.random() * 1000000) + 1;
+var User92 = Prefix+Random92
+var Random93 = Math.floor(Math.random() * 1000000) + 1;
+var User93 = Prefix+Random93
+var Random93 = Math.floor(Math.random() * 1000000) + 1;
+var User93 = Prefix+Random93
+var Random94 = Math.floor(Math.random() * 1000000) + 1;
+var User94 = Prefix+Random94
+var Random95 = Math.floor(Math.random() * 1000000) + 1;
+var User95 = Prefix+Random95
+var Random96 = Math.floor(Math.random() * 1000000) + 1;
+var User96 = Prefix+Random96
+var Random97 = Math.floor(Math.random() * 1000000) + 1;
+var User97 = Prefix+Random97
+var Random98 = Math.floor(Math.random() * 1000000) + 1;
+var User98 = Prefix+Random98
+var Random99 = Math.floor(Math.random() * 1000000) + 1;
+var User99 = Prefix+Random99
+var Random100 = Math.floor(Math.random() * 1000000) + 1;
+var User100 = Prefix+Random100
+  const bot1 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User1
+  }) 
+  const bot2 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User2
+  }) 
+  const bot3 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User3
+  })
+  const bot4 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User4
+  }) 
+  const bot5 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User5
+  }) 
+  const bot6 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User6
+  }) 
+  const bot7 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User7
+  }) 
+  const bot8 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User8
+  }) 
+  const bot9 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User9
+  }) 
+  const bot10 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User10
+  }) 
+  const bot11 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User11
+  }) 
+  const bot12 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User12
+  }) 
+  const bot13 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User13
+  }) 
+  const bot14 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User14
+  }) 
+  const bot15 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User15
+  }) 
+  const bot16 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User16
+  }) 
+  const bot17 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User17
+  }) 
+  const bot18 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User18
+  }) 
+  const bot19 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User19
+  }) 
+  const bot20 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User20
+  }) 
+  const bot21 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User21
+  }) 
+  const bot22 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User22
+  }) 
+  const bot23 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User23
+  }) 
+  const bot24 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User24
+  }) 
+  const bot25 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User25
+  }) 
+  const bot26 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User26
+  }) 
+  const bot27 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User27
+  }) 
+  const bot28 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User28
+  }) 
+  const bot29 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User29
+  }) 
+  const bot30 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User30
+  }) 
+  const bot31 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User31
+  }) 
+  const bot32 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User32
+  }) 
+  const bot33 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User33
+  }) 
+  const bot34 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User34
+  }) 
+  const bot35 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User35
+  }) 
+  const bot36 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User36
+  }) 
+  const bot37 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User37
+  }) 
+  const bot38 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User38
+  }) 
+  const bot39 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User39
+  }) 
+  const bot40 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User40
+  }) 
+  const bot41 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User41
+  }) 
+  const bot42 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User42
+  }) 
+  const bot43 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User43
+  }) 
+  const bot44 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User44
+  }) 
+  const bot45 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User45
+  }) 
+  const bot46 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User46
+  }) 
+  const bot47 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User47
+  }) 
+  const bot48 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User48
+  }) 
+  const bot49 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User49
+  }) 
+  const bot50 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User50
+  })
+  const bot51 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User51
+  })
+  const bot52 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User52
+  })
+  const bot53 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User53
+  })
+  const bot54 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User54
+  })
+  const bot55 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User55
+  })
+  const bot56 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User56
+  })
+  const bot57 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User57
+  })
+  const bot58 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User58
+  })
+  const bot59 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User59
+  })
+  const bot60 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User60
+  })
+  const bot61 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User61
+  })
+  const bot62 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User62
+  })
+  const bot63 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User63
+  })
+  const bot64 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User64
+  })
+  const bot65 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User65
+  })
+  const bot66 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User66
+  })
+  const bot67 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User67
+  })
+  const bot68 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User68
+  })
+  const bot69 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User69
+  })
+  const bot70 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User70
+  })
+  const bot71 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User71
+  })
+  const bot72 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User72
+  })
+  const bot73 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User73
+  })
+  const bot74 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User74
+  })
+  const bot75 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User75
+  })
+  const bot76 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User76
+  })
+  const bot77 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User77
+  })
+  const bot78 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User78
+  })
+  const bot79 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User79
+  })
+  const bot80 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User80
+  })
+  const bot81 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User81
+  })
+  const bot82 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User82
+  })
+  const bot83 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User83
+  })
+  const bot84 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User84
+  })
+  const bot85 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User85
+  })
+  const bot86 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User86
+  })
+  const bot87 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User87
+  })
+  const bot88 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User88
+  })
+  const bot89 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User89
+  })
+  const bot90 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User90
+  })
+  const bot91 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User91
+  })
+  const bot92 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User92
+  })
+  const bot93 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User93
+  })
+  const bot94 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User94
+  })
+  const bot95 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User95
+  })
+  const bot96 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User96
+  })
+  const bot97 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User97
+  })
+  const bot98 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User98
+  })
+  const bot99 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User99
+  })
+  const bot100 = mineflayer.createBot({
+    host: ip,
+    port: porter,
+    username: User100
+  })
